72ce7eec2ee947e784eef5b8cac45d16fcefe417
jmason32 <jmason32@ufl.edu>
2023-01-26 10:34:18 -0500

    Added MVC to app dir

---
+++ app/models/User.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Copyright (c) 2023 Daerik.com
+	This script may not be copied, reproduced or altered in whole or in part.
+	We check the Internet regularly for illegal copies of our scripts.
+	Do not edit or copy this script for someone else, because you will be held responsible as well.
+	This copyright shall be enforced to the full extent permitted by law.
+	Licenses to use this script on a single website may be purchased from Daerik.com
+	@Author: Daerik
+	*/
